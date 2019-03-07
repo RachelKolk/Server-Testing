87f3f480eed8e154de276d17a39630f119213904
RachelKolk <rach.m.obrien@gmail.com>
2019-03-07 16:54:41 -0500

    create insert function pass test

--- books/booksModel.js
+++ books/booksModel.js
@@ -1,6 +1,13 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-
+    insert,
 };
 
+async function insert(book) {
+    const [id] = await db('books').insert(book);
+
+    return db('books')
+      .where({id})
+      .first();
+}
